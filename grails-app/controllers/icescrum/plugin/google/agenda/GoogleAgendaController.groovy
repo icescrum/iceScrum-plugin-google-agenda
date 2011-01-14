48c3e3f32c85950c9ecfa0704d32d6b6489720b9
unknown <Nico@.(none)>
2011-01-14 16:14:48 +0100

    Correction bug google agenda

--- grails-app/controllers/icescrum/plugin/google/agenda/GoogleAgendaController.groovy
+++ grails-app/controllers/icescrum/plugin/google/agenda/GoogleAgendaController.groovy
@@ -1,11 +1,9 @@
 package icescrum.plugin.google.agenda
-import grails.plugins.springsecurity.Secured
-import org.icescrum.web.support.MenuBarSupport
 
 class GoogleAgendaController {
     static final id = 'googleAgenda'
     static ui = true
-    static menuBar = MenuBarSupport.productDynamicBar('is.ui.googleAgenda',id , false, 9)
+    static menuBar = [show:[visible:true,pos:0],title:'is.ui.googleAgenda']
     static window =  [title:'is.ui.googleAgenda',help:'is.ui.googleAgenda.help',toolbar:false]
     def index = {
        render template:'setAccount',
