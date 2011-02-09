bdb400583171f4d40453a4493fa7102b7fea3af3
unknown <Nico@.(none)>
2011-02-09 15:31:15 +0100

    Google agenda : Création du service d'écoute des évènements iceScrum

---
+++ grails-app/services/CalendarSynchroService.groovy
@@ -0,0 +1,32 @@
+import org.springframework.context.ApplicationListener
+import org.icescrum.core.event.IceScrumEvent
+import org.icescrum.core.event.IceScrumSprintEvent
+import org.icescrum.core.event.IceScrumReleaseEvent
+import org.icescrum.core.domain.Release
+import org.icescrum.core.domain.Sprint
+import org.icescrum.core.domain.User
+
+class CalendarSynchroService implements ApplicationListener<IceScrumEvent> {
+
+    void onApplicationEvent(IceScrumEvent e) {
+        try {
+            if(e.source instanceof Release){
+                manageReleaseEvent((Release)e.source, (User)e.doneBy, e.type)
+            }
+            else if(e.source instanceof Sprint){
+                manageSprintEvent((Sprint)e.source, (User)e.doneBy, e.type)
+            }
+        }
+        catch(Exception exception){
+            exception.printStackTrace()
+        }
+    }
+
+    private void manageReleaseEvent (Release release, User author, String type) {
+        println "Received release event: "+ release.id + " author: " + author.username
+    }
+
+    private void manageSprintEvent (Sprint sprint, User author, String type) {
+        println "Received sprint event: " + sprint.id + " author: " + author.username
+    }
+}
