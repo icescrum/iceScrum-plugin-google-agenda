7f41a7d91997535150b1132047195766854fa491
unknown <Guillaume@.(none)>
2011-03-16 16:11:53 +0100

    ajout de la licence sur le plugin google agenda

--- grails-app/controllers/icescrum/plugin/google/agenda/GoogleAgendaController.groovy
+++ grails-app/controllers/icescrum/plugin/google/agenda/GoogleAgendaController.groovy
@@ -1,3 +1,37 @@
+/*
+ * Copyright (c) 2011 BE ISI iSPlugins Université Paul Sabatier.
+ *
+ * This file is part of iceScrum.
+ *
+ * Google Agenda plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * Google Agenda plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Google Agenda plugin.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: 	Claude AUBRY (claude.aubry@gmail.com)
+ * 		Vincent BARRIER (vbarrier@kagilum.com)
+ *		Marc-Antoine BEAUVAIS (marcantoine.beauvais@gmail.com)
+ *		Vincent CARASSUS (vincentcarassus@gmail.com)
+ *		Gabriel GIL (contact.gabrielgil@gmail.com)
+ *		Julien GOUDEAUX (julien.goudeaux@orange.fr)
+ *		Guillaume JANDIN (guillaume.baz@gmail.com)
+ *		Jihane KHALIL (khaliljihane@gmail.com)
+ *		Paul LABONNE (paul.labonne@gmail.com)
+ *		Nicolas NOULLET (nicolas.noullet@gmail.com)
+ *		Bertrand PAGES (pages.bertrand@gmail.com)
+ *		Jérémy SIMONKLEIN (jeremy.simonklein@gmail.com)
+ *		Steven STREHL (steven.strehl@googlemail.com)
+ *
+ *
+ */
+
 package icescrum.plugin.google.agenda
 
 import com.google.gdata.client.calendar.CalendarService
