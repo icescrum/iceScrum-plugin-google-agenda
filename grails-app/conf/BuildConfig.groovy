123b159c6205c72c4ade707a3113566324584306
Nicolas Noullet <nnoullet@kagilum.com>
2012-01-26 12:02:40 +0100

    Màj BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -21,6 +21,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
         mavenRepo "http://repo.icescrum.org/artifactory/plugins-release/"
+        mavenRepo "http://repo.icescrum.org/artifactory/plugins-snapshot/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -28,8 +29,9 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.13'
     }
 
-    plugins {
-      compile ':maven-publisher:0.8.1'
-      compile "org.icescrum:icescrum-core:1.3.1"
+    if (environment == Environment.PRODUCTION){
+        plugins {
+            compile "org.icescrum:icescrum-core:1.5-SNAPSHOT"
+        }
     }
 }
